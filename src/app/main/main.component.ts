30c7b8c874763f215ca3a547506bb57bd7581886
Gergosz <71034104+Gergosz-2000@users.noreply.github.com>
2022-02-24 16:26:40 +0100

    kimaradt fejrész pótolva

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,3 +1,12 @@
+/*
+* File: main.component.ts
+* Author: Szivák Gergő
+* Copyright: 2022, Szivák Gergő
+* Group: Szoft II/N
+* Date: 2022-02-24
+* Github: https://github.com/gergosz-2000/angjarat
+* Licenc: GNU GPL
+*/
 import { Component, OnInit } from '@angular/core';
 import { VehiclesService } from '../shared/vehicles.service';
 import { Router } from '@angular/router';
